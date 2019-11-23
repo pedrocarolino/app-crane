68d838121f1b6be52c93c572002923235e064660
PedroCarolino <pedrocarolino@live.com>
2019-11-23 14:30:20 -0300

    teste

--- src/app/guindaste-serv.service.ts
+++ src/app/guindaste-serv.service.ts
@@ -5,22 +5,27 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class GuindasteServService {
+  private dados: any;
 
   constructor(public http: HttpClient) { }
 
   async saveRegister(register: any) {
-    return this.http.post<any>('http://localhost:3000/api/function/guindaste/atributos', register).toPromise();
+    return this.http.post<any>('http://localhost:3000/controllers/function/guindaste/atributos', register).toPromise();
   }
 
   async saveAltura(register: any) {
-    return this.http.post<any>('http://localhost:3000/api/function/guindaste/heigth', register).toPromise();
+    return this.http.post<any>('http://localhost:3000/controllers/function/guindaste/heigth', register).toPromise();
   }
 
   async saveAngulo(register: any) {
-    return this.http.post<any>('http://localhost:3000/api/function/guindaste/angle', register).toPromise();
+    return this.http.post<any>('http://localhost:3000/controllers/function/guindaste/angle', register).toPromise();
   }
 
   async saveEletroima(eletroima: any) {
-    return this.http.post<any>('http://localhost:3000/api/function/guindaste/eletroima', eletroima).toPromise();
+    return this.http.post<any>('http://localhost:3000/controllers/function/guindaste/eletroima', eletroima).toPromise();
+  }
+
+  async getStatus() {
+    return this.http.get<any>('http://localhost:3000/controllers/function/guindaste/status', this.dados).toPromise();
   }
 }
--- src/app/guindaste/guindaste.component.ts
+++ src/app/guindaste/guindaste.component.ts
@@ -15,16 +15,17 @@ export class GuindasteComponent implements OnInit {
     altura: 0,
     angulo: 0
   };
+  data = [];
   nomeOperador = null;
   eletroima = 0;
-  getAngulo = null;
-  getAltura = null;
-  getEletroima = null;
+  getAngulo = 0;
+  getAltura = 0;
+  getEletroima = 0;
 
   async submitNew() {
     const registro = {
-      altura: this.form.altura + 32,
-      angulo: this.form.angulo + 180,
+      altura: this.form.altura,
+      angulo: this.form.angulo,
       eletroimaStatus: this.eletroima,
       nomeOperador: this.nomeOperador
     };
@@ -36,17 +37,26 @@ export class GuindasteComponent implements OnInit {
       flag = 0;
       alert('Angulo invalida');
     }
-    if (registro.altura >= 0 && registro.altura <= 35) {
+    if (this.form.altura >= 0 && this.form.altura <= 35) {
       flag = 1;
-      console.log('altura valida');
+      console.log('Altura valida');
     } else {
       flag = 0;
       alert('Altura invalida');
+    }
+    if (this.nomeOperador === null) {
+      flag = 0;
+      alert('Insira seu nome');
+    } else {
+      flag = 1;
     }
     console.log(registro);
     if (flag === 1) {
       const response = await this.GuindasteService.saveRegister({registro});
     }
+  }
+  refreshAngulo(form) {
+    this.getAngulo = form.angulo;
   }
   liga() {
     this.eletroima = 1;
@@ -55,24 +65,9 @@ export class GuindasteComponent implements OnInit {
   desliga() {
     this.eletroima = 0;
   }
+
   async submitInicializar() {
-    console.log('Em desenvolvimento !!!');
-  }
-  async submitAngulo() {
-    try {
-      const angulo = this.form.angulo + 180;
-      const response = await this.GuindasteService.saveAngulo({angulo});
-    } catch (e) {
-      console.log('Nao foi possivel enviar o angulo, por causa do erro: ', e);
-    }
-  }
-  async submitAltura() {
-    try {
-      const altura = this.form.altura;
-      const response = await this.GuindasteService.saveAltura({altura});
-    } catch (e) {
-      console.log('Nao foi possivel enviar a altura , por causa do erro: ', e);
-    }
+    await console.log('Em desenvolvimento !!!');
   }
   ngOnInit(): void {
   }
